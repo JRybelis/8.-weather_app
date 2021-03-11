a62e2ae736e6ab93c787842760a7efac3f0f6503
JRybelis <72306551+JRybelis@users.noreply.github.com>
2021-03-11 11:48:31 +0200

    icons & images dynamic generation

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,46 @@
 const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img');
+
+const updateUI = (data) => {
+    
+    //assign object data to variables
+    // const cityDetails = data.cityDetails;
+    // const weather = data.weather;
+
+    //same as above, but using properties destructuring
+    const {cityDetails, weather} = data;
+
+    //update details template
+    details.innerHTML=`
+    <h5 class="my-3">${cityDetails.EnglishName}</h5>
+    <div class="my-3">${weather.WeatherText}</div>
+    <div class="display-4 my-4">
+        <span>${weather.Temperature.Metric.Value}</span>
+        <span>&deg;C</span>
+    </div>
+    `;
+
+    //update the night/day & icon images
+    const iconSource = `img/icons/${weather.WeatherIcon}.svg`
+    icon.setAttribute('src', iconSource);
+
+    let timeSource = null;
+    if(weather.IsDayTime) {
+        timeSource = 'img/day.svg';
+    } else {
+        timeSource = 'img/night.svg';
+    }
+    time.setAttribute('src', timeSource);
+    
+
+    //remove the d-none class, if present
+    if(card.classList.contains('d-none')){
+        card.classList.remove('d-none');
+    }
+}
 
 const updateCity = async (city) => {
     const cityDetails = await getCity(city);
@@ -16,6 +58,7 @@ cityForm.addEventListener('submit', e => {
 
     //update the UI with the new city
     updateCity(city)
-        .then(data => console.log(data))
+        .then(data => updateUI(data))
         .catch(error => console.log(error));
 });
+
